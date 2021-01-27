063069bfce7f283c9d5bbe7d784797215ee7b09c
Siddhant Bhargava <sidbhargava1202@gmail.com>
2021-01-27 22:43:46 +0530

    New app

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
@@ -28,5 +28,5 @@ def login_view(response):
 
 def logout_view(response):
     if response.method == "POST":
-        # logout(response)
-        return('/hospitals')
+        logout(response)
+        return redirect('/home')
---
+++ Disseases/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DisseasesConfig(AppConfig):
+    name = 'Disseases'
