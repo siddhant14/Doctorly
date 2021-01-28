4ea2cce91a5d8f2ed51da976ada58923fc7c531c
Siddhant Bhargava <sidbhargava1202@gmail.com>
2021-01-28 21:00:17 +0530

    static

--- Doctorly/settings.py
+++ Doctorly/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/app/static'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
