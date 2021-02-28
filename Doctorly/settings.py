fb044c0352780091be3af5508b76cf15f67b659a
Siddhant Bhargava <sidbhargava1202@gmail.com>
2021-02-28 12:25:36 +0530

    Environ

--- Doctorly/settings.py
+++ Doctorly/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# import environ
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,17 +80,17 @@ WSGI_APPLICATION = 'Doctorly.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-# env = environ.Env()
-
 # DATABASES = {
-#     'default': env.db('DATABASE_URL'),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+env = environ.Env()
+
+DATABASES = {
+    'default': env.db('DATABASE_URL'),
+}
 
 
 # Password validation
